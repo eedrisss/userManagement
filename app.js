5e31aed2e81506216224ca7887b5890b2ea9785e
eedrisss <imustapha30@yahoo.com>
2020-06-23 08:33:01 +0100

    corrected error

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/", function(req, res) {
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us10.api.mailchimp.com/3.0/lists/e635d1e36";
+    const url = "https://us10.api.mailchimp.com/3.0/lists/e635d1e364";
 
     const options = {
         method: "POST",
